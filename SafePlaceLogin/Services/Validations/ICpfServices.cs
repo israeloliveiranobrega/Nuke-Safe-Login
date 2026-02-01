ee21611b8e764189cc25ec5b9f6b6952476f226d
Ritto <israelmendes.developer@gmail.com>
2026-02-01 16:55:30 -0300

    clean folders

--- SafePlaceLogin/Services/Validations/ICpfServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nuke_Safe_Login.Domain.Services.Validations;
-
-namespace Nuke_Safe_Login.Services.Validations
-{
-    public interface ICpfServices
-    {
-        Task<CpfInfoResponse> ConsultCpf(string cpf);
-    }
-}
